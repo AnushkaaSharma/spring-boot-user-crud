bd199c60ca94c33e5153772b19d877abb1f6c135
Anushkaa Sharma <sanushkaa4@gmail.com>
2024-12-11 23:35:48 -0500

    initial commit

---
+++ src/test/java/com/example/SpringMVCApp/SpringMvcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMVCApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
